3e2e13e74ddeed28823007bc100675c2146b84d8
Sopanha <129533258+SuonSopanha@users.noreply.github.com>
2024-06-01 11:43:43 +0700

    change api url

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://online-task.rd-lab.work/',
   timeout: 250000,
   headers: {
     'Content-Type': 'application/json',
